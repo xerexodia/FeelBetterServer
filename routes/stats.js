1f9c4b29f01d24a28861df6b98e35b062157967a
ahlembelgacem <yogori98@gmail.com>
2023-06-18 05:05:17 +0100

    17/06/23-3

--- routes/stats.js
+++ routes/stats.js
@@ -4,20 +4,24 @@ const User = require("../models/User");
 const router = express.Router();
 //statistique
 router.post("/getSate", async (req, res) => {
-  const curr = new Date();
-  const firstday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 1));
-  const lastday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 7));
-  const { name } = req.body;
-  const user = await User.find({ name });
-  const mood = await Mood.find({
-    creatorId: user[0]._id,
-    createdAt: { $gte: firstday, $lt: lastday },
-  });
-  res.send({
-    status: "success",
-    msg: "data fetched successfully",
-    data: mood,
-  });
+  try {
+    const curr = new Date();
+    const firstday = new Date(curr.setDate(curr.getDate() - curr.getDay() - 1));
+    const lastday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 7));
+    const { name } = req.body;
+    const user = await User.find({ name });
+    const mood = await Mood.find({
+      creatorId: user[0]?._id,
+      createdAt: { $gte: firstday, $lt: lastday },
+    });
+    res.send({
+      status: "success",
+      msg: "data fetched successfully",
+      data: mood,
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 module.exports = router;
